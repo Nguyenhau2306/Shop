a324ae0e6db2cb1ce97debed7112783006e08561
Nguyen Hau <nguyenhau230699@gmail.com>
2023-01-10 13:46:37 +0700

    fist commit

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="text-primary fw-bold mb-4">Giới thiệu</h1>
+            <p className="lead mb-4">
+              Tổng quan về Apple tập đoàn công nghệ hàng đầu Thành lập từ năm
+              1976, trải qua 7 đời CEO với 6 dòng sản phẩm chính. Hơn 1,4 tỷ
+              người sử dụng và trở thành thương hiệu hàng đầu thế giới. Đó là
+              những gì cơ bản mà mọi người sẽ nghĩ khi nhắc đến tập đoàn Apple.
+              Apple Inc. là một tập đoàn chuyên về lĩnh vực công nghệ của Mỹ, có
+              trụ sở đặt tại bang California. Ngày Apple chính thức được thành
+              lập đúng vào ngày Cá tháng tư, ngày 1/4/1976, khi đó, Apple có tên
+              đầy đủ là Apple Computer, Inc. Cái tên này được sử dụng đến những
+              năm sau đó cho đến năm 2007 thì chuyển thành cái tên sử dụng hiện
+              nay là Apple Inc. Nhà sáng lập của Apple ngoài cố chủ tịch Steve
+              Jobs, người mà bất cứ ai cũng biết thì vẫn còn 2 doanh nhân khác
+              nữa là Steve Wozniak và Ronald Wayne. Hiện tại, CEO của Tập đoàn
+              Apple chính là Tim Cook.
+            </p>
+            <NavLink to="/contact" className="btn btn-outline-primary px-3">
+              Liện hệ
+            </NavLink>
+          </div>
+          <div className="col-md-6 d-flex justify-content-center">
+            <img
+              src="/assets/images/about.png"
+              alt="About Us"
+              height="400px"
+              width="400px"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,222 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Checkout = () => {
+  const state = useSelector((state) => state.addItem);
+
+  var total = 0;
+  const itemList = (item) => {
+    total = total + item.price;
+    return (
+      <li className="list-group-item d-flex justify-content-between lh-sm">
+        <div>
+          <h6 className="my-0">{item.title}</h6>
+        </div>
+        <span className="text-muted">${item.price}</span>
+      </li>
+    );
+  };
+
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row g-5">
+          <div className="col-md-5 col-lg-4 order-md-last">
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-primary">Giỏ hàng của bạn</span>
+              <span className="badge bg-primary rounded-pill">
+                {state.length}
+              </span>
+            </h4>
+            <ul className="list-group mb-3">
+              {state.map(itemList)}
+
+              <li className="list-group-item d-flex justify-content-between">
+                <span>Tổng (VND)</span>
+                <strong>${total}</strong>
+              </li>
+            </ul>
+
+            <form className="card p-2">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Promo code"
+                />
+                <button type="submit" className="btn btn-secondary">
+                  Redeem
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="col-md-7 col-lg-8">
+            <h4 className="mb-3">Địa chỉ thanh toán</h4>
+            <form className="needs-validation" novalidate="">
+              <div className="row g-3">
+                <div className="col-sm-6">
+                  <label htmlFor="firstName" className="form-label">
+                    Tên
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstName"
+                    placeholder="..."
+                    value=""
+                    required=""
+                  />
+                  <div className="invalid-feedback">
+                    Tên hợp lệ là bắt buộc.
+                  </div>
+                </div>
+
+                <div className="col-sm-6">
+                  <label htmlFor="lastName" className="form-label">
+                    Họ
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    placeholder="..."
+                    value=""
+                    required=""
+                  />
+                  <div className="invalid-feedback">Họ hợp lệ là bắt buộc.</div>
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="username" className="form-label">
+                    Tên người dùng
+                  </label>
+                  <div className="input-group has-validation">
+                    <span className="input-group-text">@</span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      placeholder="Tên đăng nhập..."
+                      required=""
+                    />
+                    <div className="invalid-feedback">
+                      Tên người dùng của bạn là bắt buộc.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="email" className="form-label">
+                    Email <span className="text-muted"></span>
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Nhập email của bạn ..."
+                  />
+                  <div className="invalid-feedback">
+                    Vui lòng nhập địa chỉ email hợp lệ! Để cập nhật thông tin
+                    vận chuyển.
+                  </div>
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="address" className="form-label">
+                    Địa chỉ nhận hàng 1
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address"
+                    placeholder="nhập địa chỉ..."
+                    required=""
+                  />
+                  <div className="invalid-feedback">
+                    Please enter your shipping address.
+                  </div>
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="address2" className="form-label">
+                    Địa chỉ nhận hàng 2{" "}
+                    <span className="text-muted">(Không bắt buộc)</span>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address2"
+                    placeholder="Nhập địa chỉ..."
+                  />
+                </div>
+
+                <div className="col-md-5">
+                  <label htmlFor="address2" className="form-label">
+                    Số điện thoại <span className="text-muted"></span>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address2"
+                    placeholder="Nhập sđt..."
+                  />
+                </div>
+              </div>
+
+              <hr className="my-4" />
+
+              <h4 className="mb-3">Hình thức thanh toán</h4>
+
+              <div className="my-3">
+                <div className="form-check">
+                  <input
+                    id="credit"
+                    name="paymentMethod"
+                    type="radio"
+                    className="form-check-input"
+                    checked=""
+                    required=""
+                  />
+                  <label className="form-check-label" htmlFor="credit">
+                    Thẻ tín dụng
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    id="debit"
+                    name="paymentMethod"
+                    type="radio"
+                    className="form-check-input"
+                    required=""
+                  />
+                  <label className="form-check-label" htmlFor="debit">
+                    Thẻ ghi nợ
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    id="paypal"
+                    name="paymentMethod"
+                    type="radio"
+                    className="form-check-input"
+                    required=""
+                  />
+                  <label className="form-check-label" htmlFor="paypal">
+                    ship COD
+                  </label>
+                </div>
+              </div>
+              <hr className="my-4" />
+
+              <button className="w-100 btn btn-primary btn-lg" type="submit">
+                Gửi
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
